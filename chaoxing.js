46a7eb822dd4f240eb34bb94cc61cd728766d727
LeoChen98 <1234567890chw@163.com>
2018-11-08 12:47:42 +0800

    增加了轮询开关，避免轮询和主函数冲突

--- chaoxing.js
+++ chaoxing.js
@@ -1,6 +1,8 @@
 var i = 1;
 var v = 1;
 
+var b = false;
+
 var a = 0;
 
 t();
@@ -8,34 +10,37 @@ t();
 setInterval(function () {
     $('.ncells a').children('h4').children('span').css("color", "blue");
 
-    if ($("#iframe", parent.document.body).contents().find('iframe').contents().find('.x-container').length > 0) {
-        console.log("检测到第" + i + "个弹题窗口");
-        $("#iframe", parent.document.body).contents().find('iframe').contents().find('.x-container').remove();
-        $("#iframe", parent.document.body).contents().find('iframe').contents().find('.x-component').remove();
-        console.log("已关闭");
-        i++;
-    }
-
-    if ($("#iframe", parent.document.body).contents().find('iframe').contents().find('.vjs-play-control')[0].title == "重播") {
-        a++;
-        console.log("检测到视频观看完成，准备跳到下一节:『" + $('.ncells a')[a].title + "』");
-        $('.ncells a')[a].click();
-        console.log("已跳转");
-        console.log("目前播放了" + v + "个视频");
-        v++;
-        setTimeout(t(), 10000);
-    } else if ($("#iframe", parent.document.body).contents().find('iframe').contents().find('.vjs-play-control')[0].title == "播放") {
-        $("#iframe", parent.document.body).contents().find('iframe').contents().find('.vjs-big-play-button').click();
-
-        if ($("#iframe", parent.document.body).contents().find('iframe').contents().find('.vjs-mute-control')[0].title == "静音") {
-            $("#iframe", parent.document.body).contents().find('iframe').contents().find('.vjs-mute-control')[0].click();
-            console.log("已静音");
+    if (b) {
+        if ($("#iframe", parent.document.body).contents().find('iframe').contents().find('.x-container').length > 0) {
+            console.log("检测到第" + i + "个弹题窗口");
+            $("#iframe", parent.document.body).contents().find('iframe').contents().find('.x-container').remove();
+            $("#iframe", parent.document.body).contents().find('iframe').contents().find('.x-component').remove();
+            console.log("已关闭");
+            i++;
         }
 
+        if ($("#iframe", parent.document.body).contents().find('iframe').contents().find('.vjs-play-control')[0].title == "重播") {
+            a++;
+            console.log("检测到视频观看完成，准备跳到下一节:『" + $('.ncells a')[a].title + "』");
+            $('.ncells a')[a].click();
+            console.log("已跳转");
+            console.log("目前播放了" + v + "个视频");
+            v++;
+            setTimeout(t(), 10000);
+        } else if ($("#iframe", parent.document.body).contents().find('iframe').contents().find('.vjs-play-control')[0].title == "播放") {
+            $("#iframe", parent.document.body).contents().find('iframe').contents().find('.vjs-big-play-button').click();
+
+            if ($("#iframe", parent.document.body).contents().find('iframe').contents().find('.vjs-mute-control')[0].title == "静音") {
+                $("#iframe", parent.document.body).contents().find('iframe').contents().find('.vjs-mute-control')[0].click();
+                console.log("已静音");
+            }
+
+        }
     }
 }, 5000);
 
 function t() {
+    b = false;
     $('.ncells a').children('h4').children('span').css("color", "blue");
     console.log("已选取" + $('.ncells a').length + "个小节,并已用蓝色标明,请检查是否有遗漏,如有遗漏,概不负责");
 
@@ -57,8 +62,9 @@ function t() {
         if ($("#iframe", parent.document.body).contents().find('iframe').contents().find('.x-container').length > 0) {
             $("#iframe", parent.document.body).contents().find('iframe').contents().find('.x-container').remove();
             $("#iframe", parent.document.body).contents().find('iframe').contents().find('.x-component').remove();
-            i++;
         }
-    },5000);
+
+        b = true;
+    }, 5000);
 }
 
